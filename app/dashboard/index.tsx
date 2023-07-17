f0873242973be2a3a9d8ae15f5ee3df258290b05
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-07-18 00:51:12 +0530

    create new dashboard and dashboard style and update react native paper

---
+++ app/dashboard/index.tsx
@@ -0,0 +1,23 @@
+import styles from "../../styles/dashboard.style";
+
+import {View, TouchableOpacity} from 'react-native';
+
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+
+import { Avatar, Button, Card, Text } from 'react-native-paper';
+
+
+
+export default function Dashboard() {
+    return (
+      <PaperProvider>
+
+        
+
+      </PaperProvider>
+    );
+  }
+  
+ 
