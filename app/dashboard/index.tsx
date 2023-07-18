0d8f74780991dfee809ef5ccbfa7d5461cd21e03
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-07-19 01:50:12 +0530

    member dashboard create and import dashboard images

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import styles from "../../styles/dashboard.style";
 
-import {View, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity, SafeAreaView, ImageBackground, ScrollView} from 'react-native';
 
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
@@ -13,19 +13,80 @@ import { Avatar, Button, Card, Text } from 'react-native-paper';
 export default function Dashboard() {
     return (
       <PaperProvider>
+        <SafeAreaView>
+          <ScrollView>
+          <View style={styles.MainContainer}>
+            <ImageBackground source={require('../../assets/images/member-dashboard.png')}
+                                style={{
+                        width: '100%',
+                        height: 900,
+          }}>
 
-<Card>
-    <Card.Title title="Card Title" subtitle="Card Subtitle"/>
-    <Card.Content>
-      <Text variant="titleLarge">Card title</Text>
-      <Text variant="bodyMedium">Card content</Text>
-    </Card.Content>
-    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-    <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
-    </Card.Actions>
-  </Card>
+
+
+          <View style={styles.MemberNavigationBar}>
+
+            <View style={styles.HomeNavigation}></View>
+
+            <View style={styles.MessageAndNotificationNavigation}>
+
+              <View style={styles.MessageBox}></View>
+              <View style={styles.NortificationBox}></View>
+
+            </View>
+
+          </View> 
+
+
+
+          <View style={styles.HeadMemberProfileBar}>
+
+            <View style={styles.MemberProfilePic}></View>
+            <View style={styles.WelcomeNameBar}>
+              <Text style={styles.welcome}>Welcome</Text>
+              <Text style={styles.name}>Anuradha Perera</Text>
+            </View>
+
+          </View>
+
+
+
+          <Text style={styles.Mortivationaltext}>You can better today by archiving your exercise goals</Text>
+
+
+          <View style={styles.specialAnnouncementAndDateBox}>
+            <View style={styles.specialAnnouncements}>
+              
+            </View>
+            <View style={styles.DateBox}>
+              <Text style={styles.Date}>29</Text>
+              <Text style={styles.Month}>July</Text>
+              <Text style={styles.Year}>2023</Text>
+            </View>
+          </View>
+
+
+
+          <Text style={styles.WorkingProgressTitle}>Working Progress</Text>
+
+
+          <View style={styles.workoutProgress}></View>
+
+
+          <View style={styles.WaterBox}>
+              <Text style={styles.WaterTitle}>Water Per Day</Text>
+              <Text style={styles.WaterLevel}>3</Text>
+              <Text style={styles.WaterMeasure}>Liters</Text>
+          </View>
+
+          
+
+          </ImageBackground>          
+          </View>
+          </ScrollView>
+        </SafeAreaView>
+
+
 
       </PaperProvider>
     );
--- styles/dashboard.style.js
+++ styles/dashboard.style.js
@@ -2,7 +2,198 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
 
+    MainContainer: {
+        flex: 1,
+        
+    },
+
+    MemberNavigationBar:{
+        width: 350,
+        backgroundColor:'#222222',
+        height:60,
+        marginLeft:13,
+        marginRight:13,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        
+    },
+
+    HomeNavigation:{
+        width:60,
+        backgroundColor:'red',
+        height:60,
+    },
+    MessageAndNotificationNavigation:{
+        width:120,
+        height:60,
+        backgroundColor:'white',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        
+    },
+
+    MessageBox:{
+        width:60,
+        height:60,
+        backgroundColor:'green',
+    },
+
+    NortificationBox:{
+        width:60,
+        height:60,
+        backgroundColor:'blue',
+    },
+
+    HeadMemberProfileBar:{
+        width: 350,
+        height: 70,
+        backgroundColor: '#222222',
+        borderRadius:50,
+        marginTop:10,
+        marginLeft:13,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+
+    MemberProfilePic:{
+        width:55,
+        height:55,
+        backgroundColor:'white',
+        justifyContent: 'center',
+        marginLeft:13,
+        borderRadius:50,
+        borderWidth:2,
+        borderColor:'#E54646',
+    },
+
+    WelcomeNameBar:{
+        width:200,
+        height:40,
+        marginLeft:5,
+    },
+
+    welcome:{
+        color:'#E54646',
+        fontSize:15,
+    },
+
+    name:{
+        color:'white',
+        fontSize:20,
+    },
+
+    Mortivationaltext:{
+        width:250,
+        height:159,
+        color:'white',
+        marginTop:20,
+        fontWeight:'bold',
+        fontSize:30,
+        marginLeft:16,
+
+    },
+
+    specialAnnouncementAndDateBox:{
+        width:350,
+        height:150,
+        marginLeft:13,
+        flexDirection:'row',
+        justifyContent: 'space-between',
+    },
+
+    specialAnnouncements:{
+        width:250,
+        height:150,
+        backgroundColor:'white',
+        borderRadius:20
+
+    },
+    DateBox:{        
+        width:90,
+        height:150,
+        backgroundColor:'#222222',
+        borderRadius:20,
+        paddingTop:10,
+        paddingBottom:10,
+        paddingLeft:10,
+        paddingRight:10,
+        alignItems: 'center',
+    },
+
+    Date:{
+        color:'white',
+        fontSize:60,
+        fontWeight:'bold',
+    },
+
+    Month:{
+        color:'#E54646',
+        fontSize:35,
+        marginBottom:5,
     
+    },
+
+    Year:{     
+        color:'#ffffff',
+    fontSize:10,
+    },
+
+    WorkingProgressTitle:{
+        width:250,
+        height:25,
+        color:'#E54646',
+        fontSize:20,
+        fontWeight:'500',
+        marginTop:20,
+        marginLeft:13
+    },
+
+    workoutProgress:{
+        width:350,
+        height:200,
+        backgroundColor:'#222222',
+        marginLeft:13,
+        marginTop:5,
+        borderRadius:15,
+    },
+
+    WaterBox:{       
+        width:120,
+        height:150,
+        backgroundColor:'#222222',
+        borderRadius:20,
+        paddingTop:20,
+        paddingBottom:10,
+        paddingLeft:10,
+        paddingRight:10,
+        alignItems: 'center',
+        marginLeft:13,
+        marginTop:20,
+    },
+
+    WaterTitle:{
+        color:'#9C9B9B',
+        fontSize:16,
+
+    },
+
+    WaterLevel:{
+        color:'#ffffff',
+        fontSize:70,
+        fontWeight:'bold',
+
+
+    },
+
+    WaterMeasure:{
+        color:'#E54646',
+    },
+
+
+
+ 
+    
+
 })
 
 export default styles
