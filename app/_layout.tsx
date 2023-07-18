45ed63597083d02c907ba69f23ed56c2e89b5ce6
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-07-18 20:14:28 +0530

    UI header remove

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -38,7 +38,7 @@ function RootLayoutNav() {
   return (
     <>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack/>
+        <Stack screenOptions={{headerShown:false}}/>
       </ThemeProvider>
     </>
   );
