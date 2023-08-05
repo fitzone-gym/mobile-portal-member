99e3a9f760c2347345e2508c4bbaf1b2cafcd8f2
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-08-05 14:59:44 +0530

    member signin page

--- app/sign-up/index.tsx
+++ app/sign-up/index.tsx
@@ -1,27 +1,58 @@
-import styles from '../../styles/home.style';
-
-import { View, Text, Image, SafeAreaView, ScrollView,TouchableOpacity } from 'react-native';
-
-import { useRouter } from 'expo-router';
+import styles from '../../styles/signin.style';
+import { View, Text,TextInput, Image, SafeAreaView,ImageBackground,TouchableOpacity} from 'react-native';
+import { Stack , useRouter} from 'expo-router';
 
 export default function Home() {
 
-    const router = useRouter() 
-
+    const router = useRouter()
     return (
         <SafeAreaView>
-            
-            <ScrollView>
-
-                <TouchableOpacity style={styles.buttonDashboard} onPress={()=>{router.push('/dashboard')}}>
-                    <Text>Sign up</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.forgetPasswordButton} onPress={()=>{router.push('/forget-password')}}>
-                    <Text>Forget password</Text>
-                </TouchableOpacity>
-            </ScrollView>
-            
+            <Stack.Screen
+                options={{
+                    title: '',
+                }}
+            />
+                <View style={styles.container}>
+                    <ImageBackground source= {require('../../assets/images/register.png')}resizeMode='cover' style={styles.backgroundImage}>
+                        <View style={styles.upper}>
+                            <View style={styles.top}>
+                                <Image style= {styles.logo}source={require('../../assets/images/logo.png')}/>
+                            </View>
+                            <View style={styles.upperdiv}>
+                                    <Text style={styles.upperTxt}>
+                                        Exercise smartly and be physically active
+                                    </Text>
+                            </View>
+                        </View>
+                        <View style={styles.bottom}>
+                            <Text style={styles.heading}>
+                                Sign In
+                            </Text>
+                            <View style={styles.signinTxt}>
+                                <Text style={styles.subContent}>User Name</Text>
+                                <TextInput style={styles.txtInput}  />
+                                <Text style={styles.subContent}>Password</Text>
+                                <TextInput style={styles.txtInput} autoCorrect={false} secureTextEntry={true} />
+                                <TouchableOpacity
+                                    onPress={()=>{
+                                        router.push('../forget-password')
+                                    }}
+                                >
+                                    <Text style={styles.pwtxt}>Forget Password ?</Text>
+                                </TouchableOpacity>
+                            </View>                            
+                            <TouchableOpacity
+                                style={styles.btn}
+                                onPress={() => {
+                                    router.push('../dashboard')
+                                }}
+                            >
+                                <Text style={styles.btnTxt}>Sign in</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </ImageBackground>
+                </View>
         </SafeAreaView>
     )
 }
+
---
+++ styles/signin.style.js
@@ -0,0 +1,93 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    
+    container: {
+        minHeight: '100%',       
+    }, 
+    backgroundImage:{
+        flex: 1,
+        justifyContent:'space-between',
+        height: '110%',
+        width: '100%',
+    },
+    upper:{
+        flex: 1,
+        alignItems: 'center',
+        // backgroundColor:'red',
+    },    
+    top: {
+        marginTop: 20,
+        marginLeft:5,
+    },
+    logo:{
+        width:100,
+        height:100,
+    },    
+    upperdiv:{
+        // backgroundColor:'black',
+        width:'70%'
+    },
+    upperTxt:{
+        fontSize:30,
+        textAlign:'center',
+        fontWeight:600,
+        color:'white'
+    },
+    bottom:{
+        alignItems: 'center',
+        backgroundColor: '#222222',
+        opacity: 0.9,
+        margin:30,
+        padding:0,
+        borderRadius:17,
+    },
+
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#E54646',
+        marginTop:10
+    },
+    signinTxt:{
+        justifyContent: 'center',
+        padding:20,
+        width:"100%",
+    },
+    subContent: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        textAlign:'left',
+        color:'white'
+    },
+    txtInput:{
+        fontSize: 14,
+        color: 'white',
+        padding:3,
+        borderBottomWidth: 1, 
+        borderColor: '#E54646',
+        marginBottom:30
+    },
+    pwtxt:{
+        color: '#E54646',
+        fontSize: 16,
+        textAlign: 'right',
+    },
+
+    btn: {
+        width: 250, 
+        height: 50,
+        marginBottom: 30,
+        backgroundColor: '#E54646',   
+        borderRadius: 30,
+        alignItems:'center',
+        justifyContent: 'center',
+    },
+    btnTxt:{
+        fontSize: 20,
+        color: 'white',
+
+    },
+})
+
+export default styles;
